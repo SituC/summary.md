4f26864ecb83ada75c7af5103ecec16aa0c99572
SituC <1181102955@qq.com>
2021-11-29 10:28:01 +0800

    feat: 增加深拷贝手写

--- exercise/深拷贝.js
+++ exercise/深拷贝.js
@@ -1,3 +1,15 @@
-const deepClone = () => {
-  
+const deepClone = (obj, hash = new WeakMap()) => {
+  if (obj == null) return obj
+  if (obj instanceof Date) return new Date(obj)
+  if (obj instanceof RegExp) return new RegExp(obj)
+  if (obj !== 'object') return obj
+  if (hash.get(obj)) return hash.get(obj)
+  const childObj = new obj.constructor()
+  hash.set(obj, childObj)
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      childObj[key] = deepClone(obj[key], hash)
+    }
+  }
+  return childObj
 }
